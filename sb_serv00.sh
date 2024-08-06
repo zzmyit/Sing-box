68c3bc836d1272671c8cc48dcad84d9a5d6fe9e0
zzmyit <71055651+zzmyit@users.noreply.github.com>
2024-08-06 14:09:56 +0800

    Update sb_serv00.sh

--- sb_serv00.sh
+++ sb_serv00.sh
@@ -14,7 +14,7 @@ reading() { read -p "$(red "$1")" "$2"; }
 
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-export UUID=${UUID:-'bc97f674-c578-4940-9234-0a1da46041b9'}
+export UUID=${UUID:-'277cc408-95e1-40f2-9caf-caa739702269'}
 export NEZHA_SERVER=${NEZHA_SERVER:-''} 
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
 export NEZHA_KEY=${NEZHA_KEY:-''} 
